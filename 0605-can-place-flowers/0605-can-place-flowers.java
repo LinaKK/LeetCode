b65c2dcaba1ef6bfebcd994eceffe5cce2097cfe
lilikim <lilikim0112@gmail.com>
2024-08-11 18:52:14 +0900

    Time: 1 ms (97.5%), Space: 45.5 MB (36.33%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -7,21 +7,31 @@ class Solution {
             if (flowerbed[0] == 0 && n <= 1) return true;
             return false;
         }
-       
-        for(int i=0; i<flowerbed.length-1; i++){
-            if(i == 0 || i == flowerbed.length-2){
-                if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
-                    n--;
-                }
-            }
 
-            else if(flowerbed[i] == 0 && flowerbed[i+1] == 0 && flowerbed[i+2] == 0){
+        for(int i=0; i<flowerbed.length; i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)){
                 n--;
                 i++;
             }
 
             if(n == 0) return true;
+
         }
+       
+        // for(int i=0; i<flowerbed.length-1; i++){
+        //     if(i == 0 || i == flowerbed.length-2){
+        //         if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
+        //             n--;
+        //         }
+        //     }
+
+        //     else if(flowerbed[i] == 0 && flowerbed[i+1] == 0 && flowerbed[i+2] == 0){
+        //         n--;
+        //         i++;
+        //     }
+
+        //     if(n == 0) return true;
+        // }
 
         return false;
     }
