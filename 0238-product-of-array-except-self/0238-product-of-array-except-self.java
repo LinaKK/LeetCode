dc7846c09d00b4a398e3a84cd66e6f7e31d70fa9
lilikim <lilikim0112@gmail.com>
2024-08-25 05:18:15 +0900

    Time: 2 ms (86.19%), Space: 55.3 MB (55.62%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,40 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] output = new int[nums.length];
-        Arrays.fill(output, 1);
+        int[] ans = new int[nums.length];
+        int curr = 1;
 
-        int product = 1;
+        Arrays.fill(ans, 1);
 
         for(int i=0; i<nums.length; i++){
-            output[i] *= product;
-            product *= nums[i];
+            ans[i] *= curr;
+            curr *= nums[i];
         }
 
-        product = 1;
-        for(int i=nums.length-1; i>=0; i--){
-            output[i] *= product;
-            product *= nums[i];
+        curr =1;
+        for(int i= nums.length-1; i>=0; i--){
+            ans[i] *= curr;
+            curr *= nums[i];
         }
 
-        return output;
-
-
-        // int[] ans = new int[nums.length];
-        // int curr = 1;
-
-        // Arrays.fill(ans, 1);
-
-        // for(int i=0; i<nums.length; i++){
-        //     ans[i] *= curr;
-        //     curr *= nums[i];
-        // }
-
-        // curr =1;
-        // for(int i= nums.length-1; i>=0; i--){
-        //     ans[i] *= curr;
-        //     curr *= nums[i];
-        // }
-
-        // return ans;
+        return ans;
     }   
 }
