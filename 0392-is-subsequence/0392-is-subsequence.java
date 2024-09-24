3b1c83b45e308b066be3722f9bafc636fc747448
lilikim <lilikim0112@gmail.com>
2024-09-24 17:36:01 +0900

    Time: 2 ms (60.02%), Space: 41 MB (97.35%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,27 +5,27 @@ class Solution {
         if(s.length() > t.length()) return false;
         if(s.length() == t.length()) return (s.equals(t) ? true : false);
         
-        // int idxS = 0;
+        int idxS = 0;
         
-        // for(int i=0; i<t.length(); i++){
-        //     if (s.charAt(idxS) == t.charAt(i))
-        //         idxS ++;
+        for(int i=0; i<t.length(); i++){
+            if (s.charAt(idxS) == t.charAt(i))
+                idxS ++;
             
-        //     if (idxS == s.length())
-        //         return true;
-        // }
-        
-        // return false;
-
-        int sIdx = 0;
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-
-        for(int i=0; i<tArray.length; i++){
-            if(sArray[sIdx] == tArray[i]) sIdx++;
-            if(sIdx == sArray.length) return true;
+            if (idxS == s.length())
+                return true;
         }
-
+        
         return false;
+
+        // int sIdx = 0;
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
+
+        // for(int i=0; i<tArray.length; i++){
+        //     if(sArray[sIdx] == tArray[i]) sIdx++;
+        //     if(sIdx == sArray.length) return true;
+        // }
+
+        // return false;
     }
 }
